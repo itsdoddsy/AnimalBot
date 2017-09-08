d7ff55f527b09fd3594643ab6a6ec0e20e1db5c9
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2017-09-08 08:35:29 +0100

    Bot is complete and functional.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+import aiohttp
+import requests
 
 client = discord.Client()
 
@@ -11,12 +13,26 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('!cat'):
-        # get cat picture
+        async with aiohttp.get('http://random.cat/meow') as r:
+            if r.status == 200:
+                js = await r.json()
+                await client.send_message(message.channel, js['file'])
 
     elif message.content.startswith('!dog'):
-        # get dog picture
+        while True:
+            try:
+                r = requests.get('https://random.dog/woof')
+                r.text.index('mp4') #separate mp4 from the rest
+            except ValueError: #if not mp4
+                await client.send_message(message.channel, 'https://random.dog/{}'.format(r.text))
+                break
+            else: #if mp4, redo r = requests.get(...)
+                continue
 
     elif message.content.startswith('cat!help'):
-        # display help message
+        await client.send_message(message.channel, '''
+it's literally super simple, all you gotta do is
+`!cat` or `!dog`
+doesn't get much easier than that buddy''')
 
 client.run('your_token_here')
